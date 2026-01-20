129808c7f05b4ed29782b5d763a54fbff69060bd
15cam <ccopeland92274@uat.edu>
2026-01-20 11:10:12 -0700

    Add files via upload

---
+++ Assingment 1.1 CSC215.cpp
@@ -0,0 +1,26 @@
+// Assingment 1.1 CSC215.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Today is goig to be our first day
+// Our first project
+// need to be fixed later
+
+#include <iostream>
+using namespace std;//multiple statements in one line
+int main()
+{
+  //data types
+    // integers; wholenumbers, no fractions
+    int x = 100;
+    // 2- doubles; numbers with fractions
+    double gpa = 3.7;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
