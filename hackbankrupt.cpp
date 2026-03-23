6fe80d5bd0faf79969323434c38a83fb668ee105
15cam <ccopeland92274@uat.edu>
2026-03-23 14:35:34 -0700

    Add files via upload

---
+++ hackbankrupt.cpp
@@ -0,0 +1,32 @@
+
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+// predeclare functions 
+
+void normBalance();// this will be normalize bank transaction
+void hackTransaction(); // this will be to hack transaction
+
+int main() {
+	// greet the user 
+	cout << "Welcome to Hack Bankrupt!" << endl;
+	//ask for  a transaction
+	cout << "Please enter your bank transaction: " << endl;
+	string transaction;
+	cin >> transaction;
+	return 0;
+}
+ 
+//function one 
+
+void normBalance();
+
+
+//function two 
+
+
+void hackTransaction();
+
