18c99d1ea7df4094a05fe0bfff74257d24d58bf9
15cam <ccopeland92274@uat.edu>
2026-03-24 11:18:19 -0700

    Add files via upload

--- hackbankrupt.cpp
+++ hackbankrupt.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -9,24 +7,93 @@ using namespace std;
 
 void normBalance();// this will be normalize bank transaction
 void hackTransaction(); // this will be to hack transaction
+void printFunds(); // this will be to print original funds
+void pass(); // this will be to pass money
+void swap(); // this will be to swap money
+void printBalance(); // this will be to print new balance
 
 int main() {
-	// greet the user 
+	// greet the user (for good user experience)
 	cout << "Welcome to Hack Bankrupt!" << endl;
-	//ask for  a transaction
-	cout << "Please enter your bank transaction: " << endl;
+	//ask user for a transaction
+	cout << "\nPlease enter your bank transaction: " << endl;
 	string transaction;
 	cin >> transaction;
 	return 0;
 }
- 
+
 //function one 
 
-void normBalance();
+void normBalance()  
+//this is my code block for the first function
+{
+
+
+
+
+
+}
+
 
 
 //function two 
 
+void hackTransaction() 
+//this is my code block for the second function
+{
 
-void hackTransaction();
 
+
+
+
+}
+
+
+//function three
+
+void printFunds()
+//this is my code block for the third function
+{
+
+
+
+
+
+
+}
+ // function four
+
+void pass()
+//this is my code block for the fourth function
+{
+
+
+
+
+
+}
+
+// function five
+
+void swap()
+//this is my code block for the fifth function
+{
+
+
+
+
+
+}
+
+// function six
+void printBalance()
+//this is my code block for the sixth function
+{
+
+
+
+
+
+
+
+}
