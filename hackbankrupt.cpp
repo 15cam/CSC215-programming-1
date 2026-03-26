df7a742fe210da16663c4e9b682320f4dbcee2dc
15cam <ccopeland92274@uat.edu>
2026-03-26 11:14:41 -0700

    Add files via upload

--- hackbankrupt.cpp
+++ hackbankrupt.cpp
@@ -5,95 +5,69 @@ using namespace std;
 
 // predeclare functions 
 
-void normBalance();// this will be normalize bank transaction
-void hackTransaction(); // this will be to hack transaction
-void printFunds(); // this will be to print original funds
-void pass(); // this will be to pass money
-void swap(); // this will be to swap money
-void printBalance(); // this will be to print new balance
+int normBalance();// this will be normalize bank transaction
+string hackTransaction(int accountFunds); // this will be to hack transaction
+
 
 int main() {
 	// greet the user (for good user experience)
-	cout << "Welcome to Hack Bankrupt!" << endl;
+	cout << "Welcome to Comerca Bank!" << endl;
 	//ask user for a transaction
 	cout << "\nPlease enter your bank transaction: " << endl;
 	string transaction;
 	cin >> transaction;
+
+	int accountFunds = 2000; // this is the original funds in the account
+
 	return 0;
 }
 
-//function one 
+//function one (THE MAIN ONE) 
 
-void normBalance()  
+int normBalance(){  
 //this is my code block for the first function
-{
+
+//integer parameters for both accounts 
+	int accountFunds = 2000; 
+	int accountBalance = 5000;
+	
+	//print orignal funds and balance for both accounts
+	cout << "Original Funds: " << accountFunds << endl; 
+	cout << "Original Balance: " << accountBalance << endl;
 
 
-
-
-
+//Print the funds and balance showing this normal transaction should change nothing
+	cout << "Funds: " << endl;
+	cout << "Balance: " << endl;
+	//pass account balance by reference to the hack transaction function
+	int num = 5000;
+	int& ref = num; // ref is referncing to variable num
+	cout << "Num: " << num << endl;
+	cout << "Ref: " << ref << endl;
+	ref = 10000;
+	cout << "Num: " << num << endl; // num after modifiying
+	cout << "Ref: " << ref << endl; // ref after modifiying
+	return 0;
+ 
 }
 
+//function two (returns to main) 
 
-
-//function two 
-
-void hackTransaction() 
+string hackTransaction(int accountFunds) 
 //this is my code block for the second function
-{
+ {
+//swap the bank balances 
+	int accountFunds = 10000; // this is the new hacked funds in the account
+
+   
+	
+	
+	// print out the new balances
+cout << "Hacking Funds: " << endl;
+cout << "Hacking Balance: " << endl;
+string hackedTransaction = "Hacking your Transaction: " + int (accountFunds);
 
 
 
-
-
-}
-
-
-//function three
-
-void printFunds()
-//this is my code block for the third function
-{
-
-
-
-
-
-
-}
- // function four
-
-void pass()
-//this is my code block for the fourth function
-{
-
-
-
-
-
-}
-
-// function five
-
-void swap()
-//this is my code block for the fifth function
-{
-
-
-
-
-
-}
-
-// function six
-void printBalance()
-//this is my code block for the sixth function
-{
-
-
-
-
-
-
-
+	return hackTransaction (0);
 }
