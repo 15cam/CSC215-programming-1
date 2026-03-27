81f607d72d34204e3b113e476a7e0ebaf7c222e7
15cam <ccopeland92274@uat.edu>
2026-03-26 18:02:13 -0700

    Add files via upload

--- hackbankrupt.cpp
+++ hackbankrupt.cpp
@@ -24,19 +24,19 @@ int main() {
 
 //function one (THE MAIN ONE) 
 
-int normBalance(){  
-//this is my code block for the first function
+int normBalance() {
+	//this is my code block for the first function
 
-//integer parameters for both accounts 
-	int accountFunds = 2000; 
+	//integer parameters for both accounts 
+	int accountFunds = 2000;
 	int accountBalance = 5000;
-	
+
 	//print orignal funds and balance for both accounts
-	cout << "Original Funds: " << accountFunds << endl; 
+	cout << "Original Funds: " << accountFunds << endl;
 	cout << "Original Balance: " << accountBalance << endl;
 
 
-//Print the funds and balance showing this normal transaction should change nothing
+	//Print the funds and balance showing this normal transaction should change nothing
 	cout << "Funds: " << endl;
 	cout << "Balance: " << endl;
 	//pass account balance by reference to the hack transaction function
@@ -48,26 +48,26 @@ int normBalance(){
 	cout << "Num: " << num << endl; // num after modifiying
 	cout << "Ref: " << ref << endl; // ref after modifiying
 	return 0;
- 
+
 }
 
 //function two (returns to main) 
 
-string hackTransaction(int accountFunds) 
+string hackTransaction(int accountFunds)
 //this is my code block for the second function
- {
-//swap the bank balances 
+{
+	//swap the bank balances 
 	int accountFunds = 10000; // this is the new hacked funds in the account
 
-   
-	
-	
+
+
+
 	// print out the new balances
-cout << "Hacking Funds: " << endl;
-cout << "Hacking Balance: " << endl;
-string hackedTransaction = "Hacking your Transaction: " + int (accountFunds);
+	cout << "Hacking Funds: " << endl;
+	cout << "Hacking Balance: " << endl;
+	string hackedTransaction = "Hacking your Transaction: " & int(accountFunds);
 
 
 
-	return hackTransaction (0);
+	return normBalance(0);
 }
