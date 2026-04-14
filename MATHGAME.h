c728a1304b6088a2c77808fa8c34684e04c3acc0
15cam <ccopeland92274@uat.edu>
2026-04-14 10:29:05 -0700

    Add files via upload

---
+++ MATHGAME.h
@@ -0,0 +1,31 @@
+// create a header file of your own and include it in this source file. 
+// The header file should contain a function declaration, and the source file should contain the function definition. Then, call the function from the main function.
+
+
+// Header file: mathgame.h
+#ifndef MATHGAME_H
+//define it to prevent multiple inclusions of the same header file
+#define MATHGAME_H
+
+
+
+// Function declarations (no logic here, just the signatures)
+
+// Function to display instructions to the player
+void instructions();
+// Function to generate a random number between 1 and 10
+int randomNumber();
+
+// Function to get the player's guess
+void playGame(); // MAIN FUNCTION TO PLAY THE GAME
+
+// Function to give a hint to the player
+void giveHint(int secretNumber);
+// Function to display the player's statistics
+void playStats();
+// Function to ask the player if they want to play again
+void asktoPlayAgain();
+
+// End of header file
+
+#endif // MATHGAME_H  
